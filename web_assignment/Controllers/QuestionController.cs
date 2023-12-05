d788188fcb67ff89ba4c1831458fc8642701ef96
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2023-12-05 18:14:55 +0300

    project files created as empty. entity framework added into project.

---
+++ web_assignment/Controllers/QuestionController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_assignment.Controllers;
+
+public class QuestionController : Controller
+{
+	public QuestionController()
+    { 
+
+	}
+}
