95db873b6ceb476c31c2a70473ae2a7baf98802b
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2023-12-05 17:00:15 +0300

    Init project

---
+++ web_assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace web_assignment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
