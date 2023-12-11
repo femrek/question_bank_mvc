775ebf6165dca8804f8729a2788e11afccca0502
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2023-12-11 16:53:05 +0300

    feat: sqlite installed completely.

--- web_assignment/Data/DataContext.cs
+++ web_assignment/Data/DataContext.cs
@@ -4,8 +4,10 @@ namespace web_assignment.Data;
 
 public class DataContext : DbContext
 {
-	public DataContext()
-	{
-	}
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+    public DbSet<QuestionModel> QuestionModels => Set<QuestionModel>();
 }
 
