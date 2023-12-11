b0152cf22c9dd11b4946023135517501b365beb9
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2023-12-11 17:15:03 +0300

    question controller filled with default functions.

--- web_assignment/Controllers/QuestionController.cs
+++ web_assignment/Controllers/QuestionController.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using web_assignment.Models;
 
 namespace web_assignment.Controllers;
 
 public class QuestionController : Controller
 {
-	public QuestionController()
-    { 
+    private readonly ILogger<HomeController> _logger;
 
+	public QuestionController(ILogger<HomeController> logger)
+    { 
+        _logger = logger;
 	}
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
