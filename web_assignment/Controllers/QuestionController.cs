e27f9c4903a9f1c7fefd7925708805ce6cd2095e
demirhany <demirhanyilal0@gmail.com>
2023-12-15 16:21:42 +0300

    question controller editor

--- web_assignment/Controllers/QuestionController.cs
+++ web_assignment/Controllers/QuestionController.cs
@@ -1,16 +1,19 @@
 ﻿using System;
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using web_assignment.Models;
+using web_assignment.Models; 
+using System;
+using web_assignment.Data;
 
 namespace web_assignment.Controllers;
 
 public class QuestionController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<QuestionController> _logger;
+    private readonly DataContext _context;
 
-	public QuestionController(ILogger<HomeController> logger)
+	public QuestionController(ILogger<QuestionController> logger, DataContext context)
     { 
+        _context = context;
         _logger = logger;
 	}
 
@@ -18,19 +21,37 @@ public class QuestionController : Controller
     {
         return View();
     }
-
+    [HttpGet]
     public IActionResult Editor()
     {
         return View();
     }
+
+    [HttpPost]
+    public IActionResult Editor(QuestionModel model)
+    {
+        if(ModelState.IsValid)
+        {
+            var question = new QuestionModel
+            {
+                title = model.title,
+                content = model.content,
+                correctOption = model.correctOption,
+                optionOne = model.optionOne,
+                optionTwo = model.optionTwo,
+                optionThree = model.optionThree
+            };
+
+            _context.QuestionModels.Add(question);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+        return View(model);
+    }
     public IActionResult Viewer()
     {
-        return View();
-    }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View();
     }
 }
